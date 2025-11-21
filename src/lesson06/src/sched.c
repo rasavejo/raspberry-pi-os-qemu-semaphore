c5ea31805ef98f783a6f6533f08f41a515afc9bd
tinyaitmess <tinhinane.ait-messaoud@univ-tlse3.fr>
2025-11-21 15:12:38 +0100

    resolve conflicts

--- src/lesson06/src/sched.c
+++ src/lesson06/src/sched.c
@@ -1,7 +1,6 @@
 #include "sched.h"
 #include "irq.h"
 #include "mm.h"
-#include "mm.h"
 #include "printf.h"
 #include "utils.h"
 
@@ -10,15 +9,11 @@ struct task_struct *current = &(init_task);
 struct task_struct *task[NR_TASKS] = {
     &(init_task),
 };
-struct task_struct *task[NR_TASKS] = {
-    &(init_task),
-};
+
 int nr_tasks = 1;
 
 void preempt_disable(void) { current->preempt_count++; }
-void preempt_disable(void) { current->preempt_count++; }
 
-void preempt_enable(void) { current->preempt_count--; }
 void preempt_enable(void) { current->preempt_count--; }
 
 void _schedule(void) {
@@ -53,41 +48,42 @@ void _schedule(void) {
         switch_to(task[next]);
         preempt_enable();
     }
+}
 
 void schedule(void) {
     current->counter = 0;
     _schedule();
 }
 
-    void switch_to(struct task_struct * next) {
-        if (current == next)
-            return;
-        struct task_struct *prev = current;
-        current = next;
-        set_pgd(next->mm.pgd);
-        cpu_switch_to(prev, next);
-    }
-    void schedule_tail(void) { preempt_enable(); }
+void switch_to(struct task_struct *next) {
+    if (current == next)
+        return;
+    struct task_struct *prev = current;
+    current = next;
+    set_pgd(next->mm.pgd);
+    cpu_switch_to(prev, next);
+}
+void schedule_tail(void) { preempt_enable(); }
 
-    void timer_tick() {
-        --current->counter;
-        if (current->counter > 0 || current->preempt_count > 0) {
-            return;
-        }
-        current->counter = 0;
-        enable_irq();
-        _schedule();
-        disable_irq();
+void timer_tick() {
+    --current->counter;
+    if (current->counter > 0 || current->preempt_count > 0) {
+        return;
     }
+    current->counter = 0;
+    enable_irq();
+    _schedule();
+    disable_irq();
+}
 
-    void exit_process() {
-        preempt_disable();
-        for (int i = 0; i < NR_TASKS; i++) {
-            if (task[i] == current) {
-                task[i]->state = TASK_ZOMBIE;
-                break;
-            }
+void exit_process() {
+    preempt_disable();
+    for (int i = 0; i < NR_TASKS; i++) {
+        if (task[i] == current) {
+            task[i]->state = TASK_ZOMBIE;
+            break;
         }
-        preempt_enable();
-        schedule();
     }
+    preempt_enable();
+    schedule();
+}
